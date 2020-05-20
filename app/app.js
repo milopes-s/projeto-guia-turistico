3922cf62d79cb33ec6ab0d65ed93ff7495849938
Ramon Moises <moises.colim@gmail.com>
2020-05-20 20:59:26 -0300

    implementando parte do mapa

--- app/app.js
+++ app/app.js
@@ -5,7 +5,8 @@
 var guia = angular.module('guia', [
    'ngRoute',
    'firebase',
-   'authService'
+   'authService',
+   'googleService'
 ]);
 
 guia.config(['$routeProvider', function ($routeProvider) {
--- app/controllers/homeCtrl.js
+++ app/controllers/homeCtrl.js
@@ -17,4 +17,24 @@ function homeCtrl($scope, authService) {
    vm.logout = function () {
       auth.logout()
    }
+   //teste do autocompet
+   $scope.user = {'from': '', 'fromLat': '', 'fromLng' : ''};
+    var options = {
+        componentRestrictions: {country: "in"}
+    };
+    var inputFrom = document.getElementById('from');
+    var autocompleteFrom = new google.maps.places.Autocomplete(inputFrom, options);
+    google.maps.event.addListener(autocompleteFrom, 'place_changed', function() {
+        var place = autocompleteFrom.getPlace();
+        $scope.user.fromLat = place.geometry.location.lat();
+        $scope.user.fromLng = place.geometry.location.lng();
+        $scope.user.from = place.formatted_address;
+    });
 }
+
+
+
+
+
+    
+
